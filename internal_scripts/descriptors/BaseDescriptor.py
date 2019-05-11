e8e9a3fb609c37545033b4e8caadde03d8c7f619
baaraban <perepichka@ucu.edu.ua>
2019-05-11 12:25:05 +0300

    Define descriptors skeleton

---
+++ internal_scripts/descriptors/BaseDescriptor.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+
+class BaseDescriptor(ABC):
+    def __init__(self):
+        super.__init__()
+
+    @abstractmethod
+    def describe(self, model_name, model):
+        pass
---
+++ internal_scripts/descriptors/LimeDescriptor.py
@@ -0,0 +1,10 @@
+from internal_scripts.descriptors.BaseDescriptor import BaseDescriptor
+
+
+class LimeDescriptor(BaseDescriptor):
+    def __init__(self):
+        super.__init__()
+        
+    def describe(self, model_name, model):
+        # TODO: implement summarization, return some sort of summary object
+        pass
