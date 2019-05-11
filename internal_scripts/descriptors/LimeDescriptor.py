8470c951cc40827a2915bdee8cdfde38ecb7b5dd
baaraban <perepichka@ucu.edu.ua>
2019-05-11 12:57:02 +0300

    Add __init__ to lime example

--- internal_scripts/descriptors/LimeDescriptor.py
+++ internal_scripts/descriptors/LimeDescriptor.py
@@ -2,6 +2,9 @@ from internal_scripts.descriptors.BaseDescriptor import BaseDescriptor
 
 
 class LimeDescriptor(BaseDescriptor):
+    def __init__(self):
+        super.__init__(self)
+        
     def describe(self, model_name, model):
         # TODO: implement summarization, return some sort of summary object
         pass
