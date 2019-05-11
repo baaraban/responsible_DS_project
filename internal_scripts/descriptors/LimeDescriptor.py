ec20ee520ebafea91c98ba986862c05a6b0f8172
baaraban <perepichka@ucu.edu.ua>
2019-05-11 12:25:05 +0300

    Define descriptors skeleton

---
+++ internal_scripts/descriptors/LimeDescriptor.py
@@ -0,0 +1,7 @@
+from internal_scripts.descriptors.BaseDescriptor import BaseDescriptor
+
+
+class LimeDescriptor(BaseDescriptor):
+    def describe(self, model_name, model):
+        # TODO: implement summarization, return some sort of summary object
+        pass
