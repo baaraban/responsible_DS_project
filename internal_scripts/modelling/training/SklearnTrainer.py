7e9de20ed24c169737bed7ea8f273ea18a9e6baa
baaraban <perepichka@ucu.edu.ua>
2019-05-11 21:28:05 +0300

    Add models evaluation

--- internal_scripts/modelling/training/SklearnTrainer.py
+++ internal_scripts/modelling/training/SklearnTrainer.py
@@ -3,18 +3,19 @@ import pickle
 import os
 
 from internal_scripts.modelling.training.abstractions.ModelTrainerBase import ModelTrainerBase
+from sklearn.metrics import classification_report
 
 
 class SklearnTrainer(ModelTrainerBase):
-    def __init__(self, model_name, model):
-        super().__init__(model_name, model)
+    def __init__(self, model_name, model, data_dict):
+        super().__init__(model_name, model, data_dict)
         self._SAVING_ROOT = f'{self._SAVING_ROOT}sklearn/'
 
     def tune(self, tune_parameters):
         pass
 
-    def fit(self, data_dict, fitting_params):
-        self.model.fit(data_dict['x_train'], data_dict['y_train'])
+    def fit(self, fitting_params):
+        self.model.fit(self.data_dict['x_train'], self.data_dict['y_train'])
         return self.model
 
     def save_model(self):
@@ -29,4 +30,5 @@ class SklearnTrainer(ModelTrainerBase):
         pickle.dump(self.model, open(filename, 'wb'))
 
     def score_model(self):
-        pass
+        y_test_prediction = self.model.predict(self.data_dict['x_test'])
+        return classification_report(self.data_dict['y_test'], y_test_prediction)
--- internal_scripts/modelling/training/abstractions/ModelTrainerBase.py
+++ internal_scripts/modelling/training/abstractions/ModelTrainerBase.py
@@ -2,10 +2,11 @@ from abc import ABC, abstractmethod
 
 
 class ModelTrainerBase(ABC):
-    def __init__(self, model_name, model):
+    def __init__(self, model_name, model, data_dict):
         self._SAVING_ROOT = 'models/'
         self.model_name = model_name
         self.model = model
+        self.data_dict = data_dict
         super().__init__()
 
     @abstractmethod
