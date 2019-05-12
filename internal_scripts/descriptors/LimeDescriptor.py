470363a529e59cfa79f403bc633ee567585189cd
Volodymyr Lut <volodymyrgalan@gmail.com>
2019-05-12 19:11:15 +0300

    Add Lime Descriptor

--- internal_scripts/descriptors/LimeDescriptor.py
+++ internal_scripts/descriptors/LimeDescriptor.py
@@ -1,10 +1,29 @@
-from internal_scripts.descriptors.BaseDescriptor import BaseDescriptor
+import lime
+import lime.lime_tabular
 
+from internal_scripts.descriptors.BaseDescriptor import BaseDescriptor
 
 class LimeDescriptor(BaseDescriptor):
     def __init__(self):
-        super.__init__()
-        
+        super().__init__()
+
     def describe(self, model_name, model, data_dict):
-        # TODO: implement summarization, return some sort of summary object
-        pass
+        print(model_name)
+        if (model_name == 'Linear SVC' or model_name == 'XGboost' or model_name == 'Random Forest Classifier'):
+            return {}
+        # Note: we currently don't have Lime implementation for this three models
+        test_observation = data_dict['x_test'].values[0]
+
+        explainer = lime.lime_tabular.LimeTabularExplainer(
+            data_dict['x_train'].values,
+            feature_names=data_dict['x_train'].columns.values.tolist(),
+            verbose=True, mode='classification')
+
+        exp = explainer.explain_instance(
+            test_observation,
+            model.predict_proba, num_features=5)
+
+        html_res = exp.show_in_notebook(show_table=True)
+        return {
+            "Predictions explanation [LIME]": html_res
+        }
