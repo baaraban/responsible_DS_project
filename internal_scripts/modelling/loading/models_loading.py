1a493e7a0cca2f6df20249311cab8ab30798d810
baaraban <perepichka@ucu.edu.ua>
2019-05-12 22:19:33 +0300

    Fix models loading

--- internal_scripts/modelling/loading/models_loading.py
+++ internal_scripts/modelling/loading/models_loading.py
@@ -1,17 +1,17 @@
-def get_saved_models():
+def get_saved_models(dataset_name):
     import os
     import pickle
     from keras.models import load_model
 
     def get_sklearn_models(path):
         sklearn_model_ending = '.sav'
-        return {f.split('.')[0]: pickle.load(open(f'{path}{f}', 'rb')) for f in os.listdir(path) if
-                f.endswith(sklearn_model_ending)}
+        return {f.split('.')[0].split('_')[0]: pickle.load(open(f'{path}{f}', 'rb')) for f in os.listdir(path) if
+                f.endswith(sklearn_model_ending) and dataset_name in f}
 
     def get_keras_models(path):
         keras_model_ending = '.h5'
-        return {f.split('.')[0]: load_model(f'{path}{f}') for f in os.listdir(path) if
-                f.endswith(keras_model_ending)}
+        return {f.split('.')[0].split('_')[0]: load_model(f'{path}{f}') for f in os.listdir(path) if
+                f.endswith(keras_model_ending) and dataset_name in f}
 
     path_to_models = 'models/'
     path_to_sklearn = f'{path_to_models}sklearn/'
